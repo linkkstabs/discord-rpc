a0773a018d06d72b6090eb6b90ab669a852a08fd
Mr. Procrastinator Sensei <160384195+linkkstabs@users.noreply.github.com>
2024-11-07 19:54:25 +0530

    Update rich-presence.js

--- Rich-Presence/rich-presence.js
+++ Rich-Presence/rich-presence.js
@@ -40,21 +40,27 @@ const initializeDiscordClient = (config) => {
 const updateRichPresence = (client) => {
   try {
     const richPresence = new Discord.RichPresence()
-      .setApplicationId(presenceConfig.applicationId)
-      .setType(presenceConfig.activityType)
-      .setURL(presenceConfig.url)
-      .setState(presenceConfig.state)
-      .setName(presenceConfig.name)
-      .setDetails(presenceConfig.details)
+      .setApplicationId(presenceConfig.1298676906941743197
+)
+      .setType(presenceConfig.WATCHING)
+      .setURL(presenceConfig."https://www.twitch.tv/mrprocrastinatorsensei")
+      .setState(presenceConfig."Serving Lord Rimuru.."
+)
+      .setName(presenceConfig."Forest of Jura"
+)
+      .setDetails(presenceConfig."Nation of Tempest"
+)
       .setStartTimestamp(Date.now())
-      .setAssetsLargeImage(presenceConfig.largeImage)
-      .setAssetsLargeText(presenceConfig.largeImageText)
-      .setAssetsSmallImage(presenceConfig.smallImage)
-      .setAssetsSmallText(presenceConfig.smallImageText);
+      .setAssetsLargeImage(presenceConfig."https://media.discordapp.net/attachments/1181461047736029206/1298685483546841088/tumblr_ddfd0c1510a5918a845f37d667721de6_0975f652_400.webp?ex=672d945b&is=672c42db&hm=90f12124aba9d5a33a5dc6ed3808ffb6fa444ca8287f8e82dd78a7117f8b1bff&animated=true&")
+      .setAssetsLargeText(presenceConfig."Diablo" 
+)
+      .setAssetsSmallImage(presenceConfig."https://media.discordapp.net/attachments/1181461047736029206/1298685367519543326/tensura-rimuru.gif?ex=672d9440&is=672c42c0&hm=9b33f9509346ef840e1256f7134c7e7c9050c0daa0dcc79297da24a1638e7e09&=&width=800&height=450")
+      .setAssetsSmallText(presenceConfig."Rimuru Tempest"
+);
 
     if (presenceConfig.buttons && presenceConfig.buttons.length > 0) {
       presenceConfig.buttons.forEach(button => {
-        richPresence.addButton(button.label, button.url);
+        richPresence.addButton(button.Carrd, button."https://mrprocrastinatorsensei.carrd.co/");
       });
     }
 
